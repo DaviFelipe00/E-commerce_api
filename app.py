ab09e7250678f4c47a4c91489cb74bdcfec262d8
DaviFelipe00 <davifelipeempregos@gmail.com>
2025-04-21 17:17:49 -0300

    Atualização de uppdate para a API

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable= False)
     description = db.Column(db.Text, nullable = True)
 
+#Método para adicionar produto com Try/except, caso de algum erro de chave e valor, retornara erro de BAD REQUEST
 @app.route('/api/product/add', methods=['POST'])
 def add_product():
     try:
@@ -30,6 +31,7 @@ def add_product():
         db.session.rollback()
         return jsonify({"message": str(f'error in {e}')})
 
+#Método para deletar um produto, uso o parametro de ID para excluir 
 @app.route('/api/product/delete/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
     product = Product.query.get(product_id)
@@ -38,6 +40,38 @@ def delete_product(product_id):
         db.session.commit()
         return jsonify({"message": "Product deleted succesfully"})
     return jsonify({"message": "Product not found"},404)
+
+#Método de retorno para dados dos produtos
+@app.route('/api/product/<int:product_id>', methods=['Get'])
+def product_description(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"},404)
+
+@app.route('/api/product/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"},400)
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+        
+    if 'price' in data:
+        product.price = data['price']
+        
+    if 'description' in data:
+        product.description = data['description']
+        
+    db.session.commit()
+    return jsonify({"message": "Product updated succesfully"})
 
 #Definindo rota raíz
 @app.route('/')
